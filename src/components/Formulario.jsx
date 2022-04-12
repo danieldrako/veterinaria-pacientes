b08103299cf083640c1be77c11a7a6d1b3fdb98a
LuisDaniel <ls.ortizdaniel@gmail.com>
2022-04-11 22:50:31 -0500

    proyecto finalizado 1.0

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,7 @@
 import {useState, useEffect} from 'react'
 import Error from './Error';
 
-
-
-const Formulario = ({pacientes, setPacientes}) => {
+const Formulario = ({pacientes, setPacientes, paciente ,setPaciente}) => {
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
   const [email, setEmail] = useState('');
@@ -11,6 +9,17 @@ const Formulario = ({pacientes, setPacientes}) => {
   const [sintomas, setSintomas] = useState('');
 
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if( Object.keys(paciente).length > 0){
+      setNombre(paciente.nombre)
+      setPropietario(paciente.propietario)
+      setEmail(paciente.email)
+      setFecha(paciente.fecha)
+      setSintomas(paciente.sintomas)
+
+    }
+  }, [paciente])
 
   const generarId = ()=> {
     const random = Math.random().toString(36).substr(2)
@@ -38,13 +47,23 @@ const Formulario = ({pacientes, setPacientes}) => {
       propietario,
       email,
       fecha,
-      sintomas,
-      id: generarId()
+      sintomas
     }
 
+    if(paciente.id){
 
+     objetoPaciente.id = paciente.id 
+     const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
 
-    setPacientes([...pacientes, objetoPaciente])
+     setPacientes(pacientesActualizados)
+     setPaciente({})
+
+    }else{
+
+      objetoPaciente.id = generarId();
+      setPacientes([...pacientes, objetoPaciente]);
+
+    }
 
     // Reiniciar el form
     setNombre('')
@@ -141,7 +160,7 @@ const Formulario = ({pacientes, setPacientes}) => {
           <input 
           type="submit" 
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 cursor-pointer transition-all"
-          value="Agregar paciente"
+          value={paciente.id ? 'Finalizar Edición' : "Agregar paciente"}
           />
 
             
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,6 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
-
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
   return (
       <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
@@ -17,6 +16,8 @@ const ListadoPacientes = ({pacientes}) => {
               <Paciente
                 key={paciente.id}
                 paciente={paciente}
+                setPaciente = {setPaciente}
+                eliminarPaciente = {eliminarPaciente}
               />
             ))}
           </>
